d1ce23ebec4a4b2557b4d88c1c6365f864fb12b1
kirill.temnenkov <kirill.temnenkov@efaaabe3-c1f8-a74a-a744-110d838b46c3>
2010-12-22 16:36:41 +0000

    restore after crash

--- trunk/src/com/temnenkov/jjbot/Helper.java
+++ trunk/src/com/temnenkov/jjbot/Helper.java
@@ -3,23 +3,31 @@ package com.temnenkov.jjbot;
 import org.jivesoftware.smack.packet.Message;
 
 public class Helper {
-	static String toString(Message msg) {
+	public static String toString(Message msg) {
 		if (msg == null)
 			return "null";
 
 		return safeStr(msg.getFrom()) + ":" + safeStr(msg.getBody());
 	}
 
-	static String safeStr(String str) {
+	public static String safeStr(String str) {
 		if (str == null)
 			return "";
 		else
 			return str;
 	}
 
-	static Message createChatMessage(String to, String body) {
+	public static Message createChatMessage(String to, String body) {
 		Message msg = new Message(to, Message.Type.chat);
 		msg.setBody(body);
 		return msg;
+	}
+	
+	public static String extractUser(String str){
+		return str;
+	}
+	
+	public static boolean isEmpty(String str){
+		return false;
 	}
 }
